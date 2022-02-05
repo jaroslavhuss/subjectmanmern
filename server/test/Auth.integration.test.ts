307ef91545e9c9584cfaa09bdd4b8a05f36afd92
JAN VOJACEK <vojacekjan@hotmail.com>
2022-02-05 11:38:57 +0100

    enhances weak password test scenario with more corner-cases

--- server/test/Auth.integration.test.ts
+++ server/test/Auth.integration.test.ts
@@ -31,10 +31,21 @@ describe('Auth controller', () => {
     });
 
     test('should return an error in case of weak password', async () => {
-        const user = generateUser();
-        user.password = 'weakPassword';
-        const response = await request.post('/auth-api/register').send(user);
-        expect(response.status).toEqual(400);
+        const user1 = generateUser();
+        user1.password = 'weakPassword';
+        const user2 = generateUser();
+        user2.password = 'weakPassword6';
+        const user3 = generateUser();
+        user3.password = 'weakPassword$';
+
+        const response1 = await request.post('/auth-api/register').send(user1);
+        expect(response1.status).toEqual(400);
+
+        const response2 = await request.post('/auth-api/register').send(user2);
+        expect(response2.status).toEqual(400);
+
+        const response3 = await request.post('/auth-api/register').send(user3);
+        expect(response3.status).toEqual(400);
     });
 
     test('should successfully register new user', async () => {
