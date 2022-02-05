9b331dea9687c9fffa6904ace0653539cb2aae5e
JAN VOJACEK <vojacekjan@hotmail.com>
2022-02-05 11:13:31 +0100

    implements test case covering user register success route

--- server/test/Auth.integration.test.ts
+++ server/test/Auth.integration.test.ts
@@ -1,12 +1,37 @@
 import app from "../src/app";
 import * as supertest from "supertest";
+// @ts-ignore
+import faker from "community-faker";
 
 // @ts-ignore
 const request = supertest.default(app);
 
+const generateUser = () => {
+    const gender = faker.name.gender();
+    const name = faker.name.firstName(gender);
+    const surname = faker.name.lastName(gender);
+    const language = faker.datatype.boolean() ? 'en' : 'cz';
+    const password = 'randomP@ssword6';
+    const form = faker.datatype.boolean() ? 'daily' : 'distant';
+    const level = faker.datatype.boolean() ? 'Bc.' : 'Ing.';
+    const email = faker.internet.email(name, surname);
+    return {
+        name, surname, form, level, language, email, password
+    };
+};
+
 describe('Auth controller', () => {
+    beforeAll(() => {
+        faker.locale = 'en';
+    });
+
     test('should return an error in case of wrong request body', async () => {
         const response = await request.post('/auth-api/register').send({});
         expect(response.status).toEqual(400);
+    });
+
+    test('should successfully register new user', async () => {
+        const response = await request.post('/auth-api/register').send(generateUser());
+        expect(response.status).toEqual(201);
     });
 });
